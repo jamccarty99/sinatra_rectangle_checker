b56f241a6340c4c5e94c4e6483461793ced4db3c
Justin McCarty <jamccarty99@gmail.com>
2018-08-21 13:04:20 -0400

    add Business Logic with Sinatra lesson

--- app.rb
+++ app.rb
@@ -1,18 +1,20 @@
 require('sinatra')
 require('sinatra/reloader')
 require('pry')
+also_reload('lib/**/*.rb')
+require('./lib/rectangle')
 
 get('/') do
-  @description = "This application will determine whether a rectangle is a square or not."
   erb(:input)
 end
 get('/output') do
   @length = params.fetch("length")
   @width = params.fetch("width")
-  if @length == @width
-    @rectangle = "This is a square."
+  rectangle = Rectangle.new(@length, @width)
+  if rectangle.square?
+    @string_to_display = "This is a square."
   else
-    @rectangle = "This is not a square."
+    @string_to_display = "This is not a square."
   end
   erb(:output)
 end
